3f131fcaeae78cc261af621e546d860cf6304f9d
DarkMindFX <eugene.ryzhov@gmail.com>
2023-07-30 16:55:37 +0200

    Adding Vocabularies DB + code to populate data

---
+++ Sources/CRUDAPI.DataProducer/VocabularyDataProducer.cs
@@ -0,0 +1,42 @@
+﻿using CRUDAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDAPI.DataProducer
+{
+    public class VocabularyDataProducerParams : IDataProducerParams
+    {
+        public string ConnectionString { get; set; }
+
+        public uint MaxLength { get; set; }
+
+        public string VocabularyName { get; set; }
+    }
+
+    public class VocabularyDataProducer : IDataProducer<string>
+    {
+        private VocabularyDataProducerParams _initParams;
+        private Random _rnd;
+        public void Init(IDataProducerParams initParams)
+        {
+            _initParams = initParams as VocabularyDataProducerParams;
+
+            if (string.IsNullOrEmpty(_initParams.ConnectionString))
+                throw new ArgumentException("ConnectionString is empty. Provide connection string to DB which contains vocabulary");
+
+            if (string.IsNullOrEmpty(_initParams.VocabularyName))
+                throw new ArgumentException("VocabularyName is not provided");
+
+            if (_initParams.MaxLength == 0)
+                throw new ArgumentException("MaxLength must be greater than 0");
+        }
+
+        public string NextValue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
